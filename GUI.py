e94c11351bedb7188bd2a75927f458bece42a475
Naveed Riaziat <nriaziat@jhu.edu>
2024-10-14 16:50:47 -0400

    GUI improvements

---
+++ GUI.py
@@ -0,0 +1,148 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import serial.serialutil
+from control_experiment import ExperimentType, SpeedMode, main
+from models import MultiIsothermModel, PseudoStaticModel, humanTissue, hydrogelPhantom
+import do_mpc
+from T3pro import T3pro
+from testbed import Testbed
+import TKinterModernThemes as TKMT
+import cv2
+
+class ControlExperimentUI(TKMT.ThemedTKinterFrame):
+    def __init__(self):
+        super().__init__("Control Experiment", "azure", "light")
+        self.exp_type_frame = self.addLabelFrame("Experiment Type")
+        self.velocity_type_frame = self.addLabelFrame("Velocity Type")
+        self.model_type_frame = self.addLabelFrame("Model Type")
+        self.material_type_frame = self.addLabelFrame("Material Type")
+        self.nextCol()
+        self.numerical_params_frame = self.addLabelFrame("MPC Settings")
+        self.files_frame = self.addLabelFrame("Log Files")
+
+
+        self.exp_type = tk.StringVar(value=ExperimentType.REAL)
+        self.adaptive_velocity = tk.StringVar(value="y")
+        self.constant_velocity = tk.StringVar()
+        self.model_name = tk.StringVar(value="multiisotherm")
+        self.material_name = tk.StringVar(value="human")
+        self.n_isotherms = tk.IntVar(value=1)
+        self.n_horizons = tk.IntVar(value=10)
+        self.qd = tk.IntVar(value=1)
+        self.qw = tk.IntVar(value=25)
+        self.save_dir = tk.StringVar()
+        self.logFile = tk.StringVar()
+        self.create_widgets()
+        self.run()
+
+    def create_widgets(self):
+        self.AccentButton("Start Experiment", command=self.start_experiment)
+
+        self.exp_type_frame.Radiobutton("Real", variable=self.exp_type, value=ExperimentType.REAL)
+        self.exp_type_frame.Radiobutton("Pre-recorded", variable=self.exp_type, value=ExperimentType.PRERECORDED)
+        self.exp_type_frame.Radiobutton("Simulated", variable=self.exp_type, value=ExperimentType.SIMULATED)
+
+        self.velocity_type_frame.Radiobutton("Adaptive Velocity", variable=self.adaptive_velocity, value="y")
+        self.velocity_type_frame.Radiobutton("Constant Velocity", variable=self.adaptive_velocity, value="n")
+
+        self.model_type_frame.Radiobutton("Multi-Isotherm", variable=self.model_name, value="multiisotherm")
+        self.model_type_frame.Radiobutton("Pseudo-Static", variable=self.model_name, value="pseudostatic")
+
+        self.material_type_frame.Radiobutton("Human Tissue", variable=self.material_name, value="human")
+        self.material_type_frame.Radiobutton("Hydrogel Phantom", variable=self.material_name, value="hydrogel")
+
+        self.numerical_params_frame.Label("Number of Isotherms")
+        self.numerical_params_frame.Entry(textvariable=self.n_isotherms)
+        self.numerical_params_frame.Label("Number of Horizons")
+        self.numerical_params_frame.Entry(textvariable=self.n_horizons)
+        self.numerical_params_frame.Label("Qd")
+        self.numerical_params_frame.Entry(textvariable=self.qd)
+        self.numerical_params_frame.Label("Qw")
+        self.numerical_params_frame.Entry(textvariable=self.qw)
+
+        self.save_indicator = self.files_frame.Button("Log Save Directory", command=self.folderDialog)
+        self.load_indicator = self.files_frame.Button("Load Log File", command=self.loadLogDialog)
+
+
+    def folderDialog(self):
+        filename = filedialog.askdirectory()
+        self.save_dir.set(filename)
+        self.save_indicator.config(text=f"Save to: {filename}")
+
+    def loadLogDialog(self):
+        filename = filedialog.askopenfilename(filetypes=[("Log Files", "*.pkl")])
+        self.logFile.set(filename)
+        self.load_indicator.config(text=f"Load from: {filename.split('/')[-1]}")
+
+
+    def start_experiment(self):
+
+        adaptive_velocity = self.adaptive_velocity.get() == "y"
+        constant_velocity = float(self.constant_velocity.get()) if not adaptive_velocity else None
+        material = humanTissue if self.material_name.get() == "human" else hydrogelPhantom
+        if self.model_name.get() == "multiisotherm":
+            if self.n_isotherms.get() < 3:
+                messagebox.showerror("Warning", "Multi-Isotherm model requires at least 3 isotherms")
+                return
+
+        model = MultiIsothermModel(n_isotherms=self.n_isotherms.get(), material=material) if self.model_name.get() == "multiisotherm" else PseudoStaticModel(material)
+
+
+        model.set_cost_function(self.qd.get(), self.qw.get())
+        model.setup()
+        mpc = do_mpc.controller.MPC(model=model)
+        ##############################
+        mpc.settings.n_horizon = self.n_horizons.get()
+        mpc.settings.n_robust = 0
+        mpc.settings.open_loop = 0
+        mpc.settings.t_step = 1 / 24
+        mpc.settings.state_discretization = 'collocation'
+        mpc.settings.collocation_type = 'radau'
+        mpc.settings.collocation_deg = 2
+        mpc.settings.collocation_ni = 2
+        mpc.settings.store_full_solution = True
+        mpc.settings.nlpsol_opts = {'ipopt.linear_solver': 'MA97',
+                                    # 'ipopt.bound_relax_factor': 0,
+                                    # 'ipopt.mu_strategy': 'adaptive',
+                                    # 'ipopt.ma57_automatic_scaling': 'yes',
+                                    # 'ipopt.check_derivatives_for_naninf': 'yes',
+                                    # 'ipopt.expect_infeasible_problem': 'yes',
+                                    # 'monitor': 'nlp_g'
+                                    }
+        # mpc.settings.supress_ipopt_output()
+        exp_type = ExperimentType(self.exp_type.get())
+        t3 = None
+        tb = None
+
+        if exp_type == ExperimentType.PRERECORDED:
+            if self.logFile.get() == "":
+                messagebox.showerror("Warning", "No log file selected")
+                return
+
+        elif exp_type == ExperimentType.REAL:
+            try:
+                t3 = T3pro()
+            except serial.serialutil.SerialException:
+                messagebox.showerror("Error", "No T3pro found")
+                return
+            try:
+                tb = Testbed()
+            except serial.serialutil.SerialException:
+                messagebox.showerror("Error", "No Testbed found")
+                return
+
+
+        if (save_dir:=self.save_dir.get()) == "":
+            messagebox.showinfo("Warning", "No save directory selected, data will not be saved")
+
+        main(model=model, mpc=mpc, exp_type=exp_type, adaptive_velocity=adaptive_velocity, constant_velocity=constant_velocity,
+            log_save_dir=save_dir, log_file_to_load=self.logFile.get(), t3=t3, tb=tb)
+        if t3 is not None:
+            t3.release()
+        if tb is not None:
+            tb.stop()
+
+
+
+if __name__ == "__main__":
+    app = ControlExperimentUI()
--- control_experiment.py
+++ control_experiment.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from enum import StrEnum
 import pandas as pd
 import warnings
+from typing import Optional
 warnings.filterwarnings("ignore")
 
 class ExperimentType(StrEnum):
@@ -25,82 +26,69 @@ r = 0.001  # control change cost
 v_min = 0  # minimum velocity [mm/s]
 v_max = 10  # maximum velocity [mm/s]
 
-def mpc_loop(mpc, u0, frame, init_mpc, b=None):
-    defl_px, _ = tipPos.update_with_measurement(frame)
-    defl_mm = defl_px / thermal_px_per_mm
-    w_mm = np.array([(cv_isotherm_width(frame, temp)[0] / thermal_px_per_mm) for temp in model.isotherm_temps])
-    model._isotherm_width_measurement = w_mm
-    deflection_adaptation.update(defl_mm, np.exp(-model.c_defl / u0))
-    u_prime = (2 * model._material.alpha / u0) *np.sqrt(
-        u0 / (4 * np.pi * model._material.k * model._material.alpha * (model.t_death - model.Ta)))
-    thermal_adaptation.update(w_mm[0], u_prime)
-    if b is not None:
-        deflection_adaptation.b = b
-    if all(w_mm > 0) and len(w_mm) == len(set(w_mm)):
-        if not init_mpc:
-            mpc.x0 = w_mm
-            mpc.set_initial_guess()
-            init_mpc = True
-        u0 = mpc.make_step(w_mm).item()
-        # lb_violation = mpc.opt_x_num.cat <= mpc._lb_opt_x.cat
-        # ub_violation = mpc.opt_x_num.cat >= mpc._ub_opt_x.cat
-        # opt_labels = mpc.opt_x.labels()
-        # labels_lb_viol = np.array(opt_labels)[np.where(lb_violation)[0]]
-        # labels_ub_viol = np.array(opt_labels)[np.where(ub_violation)[0]]
-        # print(f"Lower bound violation: {labels_lb_viol}")
-        # print(f"Upper bound violation: {labels_ub_viol}")
-        # if len(labels_lb_viol) > 0 and len(labels_ub_viol) > 0:
-        #     input()
-        plotter.plot()
-        plt.pause(0.0001)
-    return u0, init_mpc, defl_mm
+def main(model,
+         mpc,
+         exp_type:ExperimentType,
+         adaptive_velocity:bool,
+         constant_velocity:float,
+         log_save_dir:str,
+         log_file_to_load:str,
+         t3:Optional[T3pro]=None,
+         tb:Optional[Testbed]=None):
 
-def add_to_data_log(data_log: pd.DataFrame, thermal_arr: np.array,
-                    widths_mm: np.array,
-                    deflection_mm: float,
-                    adaptive_deflection_model: ScalarLinearAlgabraicAdaptation,
-                    adaptive_velocity: bool = False,
-                    const_velocity: float = None):
+    def add_to_data_log(data_log: pd.DataFrame, thermal_arr: np.array,
+                        widths_mm: np.array,
+                        deflection_mm: float,
+                        adaptive_deflection_model: ScalarLinearAlgabraicAdaptation,
+                        adaptive_velocity: bool = False,
+                        const_velocity: float = None):
 
-    data_log.loc[len(data_log)] = [widths_mm, mpc.u0 if adaptive_velocity else const_velocity, deflection_mm, thermal_arr, adaptive_deflection_model.b]
+        data_log.loc[len(data_log)] = [widths_mm, mpc.u0 if adaptive_velocity else const_velocity, deflection_mm,
+                                       thermal_arr, adaptive_deflection_model.b]
 
-
-if __name__ == "__main__":
+    def mpc_loop(mpc, u0, frame, init_mpc, b=None):
+        defl_px, _ = tipPos.update_with_measurement(frame)
+        defl_mm = defl_px / thermal_px_per_mm
+        w_mm = np.array([(cv_isotherm_width(frame, temp)[0] / thermal_px_per_mm) for temp in model.isotherm_temps])
+        model._isotherm_width_measurement = w_mm
+        deflection_adaptation.update(defl_mm, np.exp(-model.c_defl / u0))
+        u_prime = (2 * model._material.alpha / u0) * np.sqrt(
+            u0 / (4 * np.pi * model._material.k * model._material.alpha * (model.t_death - model.Ta)))
+        thermal_adaptation.update(w_mm[0], u_prime)
+        if b is not None:
+            deflection_adaptation.b = b
+        if all(w_mm > 0) and len(w_mm) == len(set(w_mm)):
+            if not init_mpc:
+                mpc.x0 = w_mm
+                mpc.set_initial_guess()
+                init_mpc = True
+            u0 = mpc.make_step(w_mm).item()
+            # lb_violation = mpc.opt_x_num.cat <= mpc._lb_opt_x.cat
+            # ub_violation = mpc.opt_x_num.cat >= mpc._ub_opt_x.cat
+            # opt_labels = mpc.opt_x.labels()
+            # labels_lb_viol = np.array(opt_labels)[np.where(lb_violation)[0]]
+            # labels_ub_viol = np.array(opt_labels)[np.where(ub_violation)[0]]
+            # print(f"Lower bound violation: {labels_lb_viol}")
+            # print(f"Upper bound violation: {labels_ub_viol}")
+            # if len(labels_lb_viol) > 0 and len(labels_ub_viol) > 0:
+            #     input()
+            plotter.plot()
+            plt.pause(0.0001)
+        return u0, init_mpc, defl_mm
 
     # Initialize the physical models
-    model = MultiIsothermModel(n_isotherms=3,
-                               material=hydrogelPhantom)
+    # model = MultiIsothermModel(n_isotherms=3,
+    #                            material=hydrogelPhantom)
     # model = PseudoStaticModel(material=hydrogelPhantom)
-    model.set_cost_function(qw, qd)
-    model.setup()
+    # model.set_cost_function(qw, qd)
+    # model.setup()
     tipPos = ToolTipKF(0.7)
 
     ## Initialize the parameter adaptation
     thermal_adaptation = ScalarLinearAlgabraicAdaptation(b=45, gamma=0.5)
     deflection_adaptation = ScalarLinearAlgabraicAdaptation(b=0.1, gamma=0.5)
 
-    ## Initialize the MPC controller
-    mpc = do_mpc.controller.MPC(model=model)
 
-    ##############################
-    mpc.settings.n_horizon = 5
-    mpc.settings.n_robust = 0
-    mpc.settings.open_loop = 0
-    mpc.settings.t_step = 1/24
-    mpc.settings.state_discretization = 'collocation'
-    mpc.settings.collocation_type = 'radau'
-    mpc.settings.collocation_deg = 2
-    mpc.settings.collocation_ni = 2
-    mpc.settings.store_full_solution = True
-    mpc.settings.nlpsol_opts = {'ipopt.linear_solver': 'MA97',
-                                # 'ipopt.bound_relax_factor': 0,
-                                # 'ipopt.mu_strategy': 'adaptive',
-                                # 'ipopt.ma57_automatic_scaling': 'yes',
-                                # 'ipopt.check_derivatives_for_naninf': 'yes',
-                                # 'ipopt.expect_infeasible_problem': 'yes',
-                                # 'monitor': 'nlp_g'
-                                }
-    # mpc.settings.supress_ipopt_output()
     #############################
 
 
@@ -130,26 +118,19 @@ if __name__ == "__main__":
     ## Setup plotting
     plotter = Plotter(mpc.data, isotherm_temps=model.isotherm_temps)
 
-    exp_type = input("Real, Pre-recorded, or Simulated? (r/p/s): ").lower().strip()
+    # exp_type = input("Real, Pre-recorded, or Simulated? (r/p/s): ").lower().strip()
     exp_type = ExperimentType(exp_type)
     if exp_type == ExperimentType.REAL:
-        adaptive_velocity = input("Adaptive Velocity? (y/n): ").lower().strip()
-        constant_velocity = None
-        if adaptive_velocity == "n":
-            constant_velocity = float(input("Enter constant velocity: "))
+        if not adaptive_velocity:
+            # constant_velocity =
+            # constant_velocity = float(input("Enter constant velocity: "))
             print(f"Constant velocity: {constant_velocity} mm/s")
             speed_mode = SpeedMode.CONSTANT
         else:
             speed_mode = SpeedMode.ADAPTIVE
 
-        t3 = T3pro(port=2)
-        tb = Testbed()
-    else:
-        t3 = None
-        tb = None
-
     if exp_type == ExperimentType.PRERECORDED:
-        data = pkl.load(open('logs/data_adaptive_2024-09-17-14:50.pkl', 'rb'))
+        data = pkl.load(open(log_file_to_load, 'rb'))
         if isinstance(data, LoggingData):
             thermal_frames = data.thermal_frames
             bs = data.damping_estimates
@@ -183,6 +164,8 @@ if __name__ == "__main__":
         for i in range(n_steps):
             u0 = mpc.make_step(x0)
             defl = deflection_adaptation.b * np.exp(-model.c_defl / u0.item()) * np.random.normal(1, 0.05)
+            if defl < 0:
+                defl = 0
             deflection_adaptation.update(defl, u0.item())
             u_prime = (2 * model._material.alpha / u0) * np.sqrt(
                 u0 / (4 * np.pi * model._material.k * model._material.alpha * (model.t_death - model.Ta)))
@@ -195,7 +178,6 @@ if __name__ == "__main__":
 
     elif exp_type == ExperimentType.REAL:
         u0 = (v_min + v_max) / 2
-        # data_log = {'widths_mm': [], 'velocities': [], 'deflections_mm': [], 'thermal_frames': [], 'positions_mm': [], 'damping_estimates': []}
         data_log = pd.DataFrame(columns=['widths_mm', 'velocities', 'deflections_mm', 'thermal_frames', 'damping_estimates'])
         home_input = input("Press Enter to home the testbed or 's' to skip: ")
         if home_input != 's':
@@ -243,8 +225,5 @@ if __name__ == "__main__":
         t3.release()
     if exp_type == ExperimentType.REAL and len(data_log['widths_mm']) > 0:
         date = datetime.now()
-        fname = f"logs/data_{str(speed_mode)}_{date.strftime('%Y-%m-%d-%H:%M')}.pkl"
+        fname = f"{log_save_dir}/data_{str(speed_mode)}_{date.strftime('%Y-%m-%d-%H:%M')}.pkl"
         data_log.to_pickle(fname)
-
-
-
